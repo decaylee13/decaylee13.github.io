ab6c65cdb99370680a8cafea596545117346ad85
Decaylee13 <dl2635@princeton.edu>
2025-12-05 01:10:22 -0500

    edits

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // Change this to use relative paths
+  base: './', // Use relative paths for assets
+  build: {
+    outDir: 'docs', // Output build to /docs for GitHub Pages
+  },
   plugins: [react()],
   assetsInclude: ['**/*.pdf']
 })
